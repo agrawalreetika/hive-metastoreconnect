4388bffa25d346db3b1fbce29b0a673587024560
Reetika Agrawal <reetika.agrawal@walmart.com>
2019-02-12 08:51:09 +0530

    Metastore client to connect to hive metastore

---
+++ src/main/java/Main.java
@@ -0,0 +1,50 @@
+/**
+ * Created by ragraw1 on 08/02/19.
+ */
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.metastore.api.MetaException;
+import org.apache.hadoop.hive.metastore.HiveMetaStoreClient;
+import org.apache.hadoop.hive.ql.session.SessionState;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.io.File;
+
+
+import org.apache.hadoop.fs.Path;
+
+import org.apache.hadoop.security.UserGroupInformation;
+
+public class Main
+{
+    public static void  main(String [] args) throws MetaException,MalformedURLException,IOException
+    {
+        System.setProperty("java.security.krb5.conf", "/Users/ragraw1/kerberos/dev17/krb5.conf");
+
+        Configuration configuration = new Configuration();
+        configuration.set("hadoop.security.authentication", "kerberos");
+        configuration.addResource("/Users/ragraw1/kerberos/dev17/hdfs-site.xml");
+
+        UserGroupInformation.setConfiguration(configuration);
+        UserGroupInformation.loginUserFromKeytab("ragraw1"+"@"+
+                        "HADOOP_DEV17.WAL-MART.COM",
+                "/Users/ragraw1/kerberos/.ragraw1.keytab");
+
+        HiveConf hiveConf = new HiveConf(SessionState.class);
+        hiveConf.addResource(new Path("/Users/ragraw1/kerberos/dev17/hive-site.xml"));
+
+        //hiveConf.setVar(HiveConf.ConfVars.METASTOREURIS, "thrift://tstr400080.srvdev1.sdc.wal-mart.com:9083,thrift://tstr400186.wal-mart.com:9083");
+        //hiveConf.setVar(HiveConf.ConfVars.HIVE_SERVER2_AUTHENTICATION, "KERBEROS");
+
+        HiveMetaStoreClient hiveMetaStoreClient = new HiveMetaStoreClient(hiveConf);
+
+        System.out.println("Metastore client : " + hiveMetaStoreClient);
+        System.out.println(hiveMetaStoreClient.getAllTables("ragraw1"));
+
+        hiveMetaStoreClient.close();
+    }
+
+}
+
